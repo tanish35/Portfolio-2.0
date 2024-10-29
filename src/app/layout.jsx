8932257b268234bd9c7f60c789fddedba232a79d
tanish35 <tanishmajumdar2912@gmail.com>
2024-10-29 11:37:21 +0530

    Add favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -27,6 +27,43 @@ export const metadata = {
     "AI expert",
   ],
   robots: "index, follow",
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        sizes: "16x16",
+        url: "/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        sizes: "32x32",
+        url: "/favicon/favicon-32x32.png",
+      },
+    ],
+    appleTouchIcon: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        url: "/favicon/apple-touch-icon.png",
+      },
+    ],
+    androidIcon: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        url: "/favicon/android-chrome-192x192.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "512x512",
+        url: "/favicon/android-chrome-512x512.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -38,11 +75,6 @@ export default function RootLayout({ children }) {
       <Script id="google-script" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${process.env.NEXT_PUBLIC_GA4}');`}
       </Script>
-      <Script
-        strategy="afterInteractive"
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4}`}
-      ></Script>
       <body className={inter.className}>
         <NavBar />
         {children}
